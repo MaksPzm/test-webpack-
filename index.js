81c523cc6bda7e8e9b558b50a595e938c45a2ea6
MaksPzm <makspzm@gmail.com>
2025-05-19 00:38:48 +0500

    start

---
+++ index.js
@@ -0,0 +1,2 @@
+import "/script/script.js";
+import "/scss/main.scss";
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: '/index.js',
+    output: {
+        filename: 'main.js'
+
+    },
+    module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+}
